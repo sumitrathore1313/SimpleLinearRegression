d7ca5552f606bed33b8848b74c756d131128c676
sumit rathore <sumitrathore1313@gmail.com>
2017-06-21 01:07:36 +0530

    modified:   test.txt 	modified:   text.py

--- text.py
+++ text.py
@@ -1,26 +1,26 @@
+# this is simple linear regression basic programe
 import pandas as pd
 from sklearn import linear_model
 import matplotlib.pyplot as plt
 import numpy as np
 
 #read dataset
-dataframe = pd.read_csv('test.txt', sep=",", header=None, names=["key", "value"])
+dataframe = pd.read_csv('challenge_dataset.txt', sep=",", header=None, names=["key", "value"])
 x_frame = dataframe[['key']]
 y_frame = dataframe[['value']]
 
+x_mean = np.mean(x_frame, dtype=float)
+y_mean = np.mean(y_frame, dtype=float)
 
-reg = linear_model.LinearRegression()
-reg.fit(x_frame, y_frame)
+a = np.sum(np.multiply(x_frame-x_mean, y_frame-y_mean))/np.sum((x_frame - x_mean)**2)
+b = np.subtract(y_mean , a*x_mean)
+y_predict = a*x_frame + b['value']
+
+RMSE = np.sqrt(np.sum(np.subtract(y_predict,y_frame)**2)/len(y_frame))
+
+print RMSE
 
 plt.scatter(x_frame, y_frame)
-plt.plot(x_frame, reg.predict(x_frame))
+plt.plot(x_frame, y_predict)
+
 plt.show()
-
-error = reg.predict(x_frame)
-error = np.squeeze(error)
-
-loss = y_frame['value'] - error
-
-total_loss = loss.sum()
-
-print total_loss
