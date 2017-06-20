3184ca92b5b7a8bfa98e2ecab41630f5a8620c9f
sumit rathore <sumitrathore1313@gmail.com>
2017-06-20 13:44:33 +0530

    t

---
+++ skr_solution.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sklearn import linear_model
+import matplotlib.pyplot as plt
+import numpy as np
+
+#read dataset
+dataframe = pd.read_csv('challenge_dataset.txt', sep=",", header=None, names=["key", "value"])
+x_frame = dataframe[['key']]
+y_frame = dataframe[['value']]
+
+
+reg = linear_model.LinearRegression()
+reg.fit(x_frame, y_frame)
+
+plt.scatter(x_frame, y_frame)
+plt.plot(x_frame, reg.predict(x_frame))
+plt.show()
+
+error = reg.predict(x_frame)
+error = np.squeeze(error)
+
+loss = y_frame['value'] - error
+
+total_loss = np.sum(loss)
+
+print total_loss
